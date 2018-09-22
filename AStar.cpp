b245e9c63dd06d02285e7ccacb9c4b3dbcd0406a
Julio Berina <julioberina@gmail.com>
2018-09-21 20:52:24 -0700

    Create AStar class with prototype implementations

---
+++ AStar.cpp
@@ -0,0 +1,22 @@
+#include "AStar.hpp"
+
+void AStar::solveH1()
+{
+
+}
+
+void AStar::solveH2()
+{
+  
+}
+
+// Search methods
+bool search(priority_queue<Board, vector<Board>, compareH1>& pq)
+{
+
+}
+
+bool search(priority_queue<Board, vector<Board>, compareH2>& pq)
+{
+
+}
---
+++ AStar.hpp
@@ -0,0 +1,37 @@
+# pragma once
+#include <queue>
+#include <cstddef>
+#include "Board.hpp"
+using namespace std;
+
+class AStar
+{
+private:
+  int globalDepth;
+
+  struct compareH1
+  {
+    bool operator()(const Board& left, const Board& right)
+    {
+      return (left.hamming() + globalDepth) > (right.hamming() + globalDepth);
+    }
+  };
+
+  struct compareH2
+  {
+    bool operator()(const Board& left, const Board& right)
+    {
+      return (left.manhattan() + globalDepth) > (right.manhattan() + globalDepth);
+    }
+  };
+
+  int cost;
+  Board initial;
+  Board finish;
+  bool search(priority_queue<Board, vector<Board>, compareH1>& pq);
+  bool search(priority_queue<Board, vector<Board>, compareH2>& pq);
+public:
+  AStar(Board& init) : initial(init), finish(NULL) { initial = init; cost = 0; }
+  void solveH1(); // Solve using Hamming
+  void solveH2(); // Solve using Manhattan
+};
